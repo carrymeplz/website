51fcec287a93eb8cf92544a25fd760eaf9056ab4
Jason <jasonccmfry@gmail.com>
2018-08-23 22:39:06 -0700

    Cleaned up ur tabs Ryan

--- js/list.js
+++ js/list.js
@@ -1,62 +1,62 @@
 $(document).ready(function() {
 
-	var queryString = decodeURIComponent(window.location.search);
-	queryString = queryString.substring(1);
-	var queries = queryString.split("&");
-	for (var i = 0; i < queries.length; i++)
-	{
-		if (i == 0) {
-			firebase.database().ref(queries[i].split('=')[1]).once('value').then(function(snapshot) {
-		        var summoners = snapshot.val();
-		        for (var key in summoners) {
-					var row = "<tr>";
-		        	row = row.concat("<td>" + summoners[key].userIgn + "</td>");
-		        	row = row.concat("<td><img src='http://ddragon.leagueoflegends.com/cdn/8.16.1/img/profileicon/" + summoners[key].profileIconId + ".png'/></td>");
-		        	row = row.concat("<td><img src='./pictures/tier/" + tierDict[decodeTier(summoners[key].soloRank)] + ".png'/></td>");
-		        	row = row.concat("<td>" + tierDict[decodeTier(summoners[key].soloRank)] + "</td>");
-		        	row = row.concat("<td>" + decodeRank(summoners[key].soloRank) + "</td>");
-		        	row = row.concat("<td><img src='./pictures/tier/" + tierDict[decodeTier(summoners[key].flexRank)] + ".png'/></td>");
-		        	row = row.concat("<td>" + tierDict[decodeTier(summoners[key].flexRank)] + "</td>");
-		        	row = row.concat("<td>" + decodeRank(summoners[key].flexRank) + "</td>");
-		        	row = row.concat("<td>" + summoners[key].flexRankWins + "</td>");
-		        	row = row.concat("<td>" + summoners[key].flexRankLosses + "</td>");
-		        	row = row.concat("<td>" + Math.round(summoners[key].flexRankWins / (summoners[key].flexRankWins + summoners[key].flexRankLosses) * 100) + "</td>");
-		        	row = row.concat("<td>" + summoners[key].soloRankWins + "</td>");
-		        	row = row.concat("<td>" + summoners[key].soloRankLosses + "</td>");
-		        	row = row.concat("<td>" + Math.round(summoners[key].soloRankWins / (summoners[key].soloRankWins + summoners[key].soloRankLosses) * 100) + "</td>");
-		        	row = row.concat(appendChecked(summoners[key].roles));
-		        	row = row.concat(appendChecked(summoners[key].gameType));
-					if (summoners[key].grindorfun == 2)
-			        	row = row.concat("<td>grind/fun</td>");
-		            else if (summoners[key].grindorfun == 1)
-			        	row = row.concat("<td>grind</td>");
-		            else
-			        	row = row.concat("<td>fun</td>");
-		            if (summoners[key].micAvail == 1)
-			        	row = row.concat("<td>yes</td>");
-		            else
-			        	row = row.concat("<td>no</td>");
-			        row = row.concat("<td>" + summoners[key].notes + "</td>")
-		            row = row.concat("</tr>");
-		            //console.log(row);
-				    $('#summonersList').find('tbody').append(row);
-		        }
-		    });
-		}
-		console.log(queries[i]);
-	}
+    var queryString = decodeURIComponent(window.location.search);
+    queryString = queryString.substring(1);
+    var queries = queryString.split("&");
+    for (var i = 0; i < queries.length; i++)
+    {
+        if (i == 0) {
+            firebase.database().ref(queries[i].split('=')[1]).once('value').then(function(snapshot) {
+                var summoners = snapshot.val();
+                for (var key in summoners) {
+                    var row = "<tr>";
+                    row = row.concat("<td>" + summoners[key].userIgn + "</td>");
+                    row = row.concat("<td><img src='http://ddragon.leagueoflegends.com/cdn/8.16.1/img/profileicon/" + summoners[key].profileIconId + ".png'/></td>");
+                    row = row.concat("<td><img src='./pictures/tier/" + tierDict[decodeTier(summoners[key].soloRank)] + ".png'/></td>");
+                    row = row.concat("<td>" + tierDict[decodeTier(summoners[key].soloRank)] + "</td>");
+                    row = row.concat("<td>" + decodeRank(summoners[key].soloRank) + "</td>");
+                    row = row.concat("<td><img src='./pictures/tier/" + tierDict[decodeTier(summoners[key].flexRank)] + ".png'/></td>");
+                    row = row.concat("<td>" + tierDict[decodeTier(summoners[key].flexRank)] + "</td>");
+                    row = row.concat("<td>" + decodeRank(summoners[key].flexRank) + "</td>");
+                    row = row.concat("<td>" + summoners[key].flexRankWins + "</td>");
+                    row = row.concat("<td>" + summoners[key].flexRankLosses + "</td>");
+                    row = row.concat("<td>" + Math.round(summoners[key].flexRankWins / (summoners[key].flexRankWins + summoners[key].flexRankLosses) * 100) + "</td>");
+                    row = row.concat("<td>" + summoners[key].soloRankWins + "</td>");
+                    row = row.concat("<td>" + summoners[key].soloRankLosses + "</td>");
+                    row = row.concat("<td>" + Math.round(summoners[key].soloRankWins / (summoners[key].soloRankWins + summoners[key].soloRankLosses) * 100) + "</td>");
+                    row = row.concat(appendChecked(summoners[key].roles));
+                    row = row.concat(appendChecked(summoners[key].gameType));
+                    if (summoners[key].grindorfun == 2)
+                        row = row.concat("<td>grind/fun</td>");
+                    else if (summoners[key].grindorfun == 1)
+                        row = row.concat("<td>grind</td>");
+                    else
+                        row = row.concat("<td>fun</td>");
+                    if (summoners[key].micAvail == 1)
+                        row = row.concat("<td>yes</td>");
+                    else
+                        row = row.concat("<td>no</td>");
+                    row = row.concat("<td>" + summoners[key].notes + "</td>")
+                    row = row.concat("</tr>");
+                    //console.log(row);
+                    $('#summonersList').find('tbody').append(row);
+                }
+            });
+        }
+        console.log(queries[i]);
+    }
 
-	function appendChecked(datas) {
-		var row = "<td>";
-	    for (var data in datas) {
-	        if (datas[data])
-	        	row = row.concat(data + "\n");
-	    }
-	    row = row.concat("</td>");
-	    return row;
-	}
+    function appendChecked(datas) {
+        var row = "<td>";
+        for (var data in datas) {
+            if (datas[data])
+                row = row.concat(data + "\n");
+        }
+        row = row.concat("</td>");
+        return row;
+    }
 
-	function applyFilter(summoner) {
-		summoner.soloRank;
-	}
+    function applyFilter(summoner) {
+        summoner.soloRank;
+    }
 })
